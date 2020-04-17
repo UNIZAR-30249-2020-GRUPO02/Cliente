d14c6c32e64b650df7c52a7f344e5922c5cdfcc8
Joaquin15 <joaquinpuyuelolardies@gmail.com>
2020-04-17 19:38:07 +0200

    Avances en implementación de la GUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReservaComponent } from './reserva/reserva.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'reserva', component: ReservaComponent },
+  { path: 'app', component: AppComponent },
+  { path: '',
+    redirectTo: '/reserva',
+    pathMatch: 'full'
+  },
+  /*{
+    path: '**', component: PageNotFoundComponent
+  }*/
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservaComponent,
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
