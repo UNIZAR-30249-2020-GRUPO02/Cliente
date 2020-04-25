651a1318209897e39d7a176646e97faa4e47bf8c
Joaquin15 <joaquinpuyuelolardies@gmail.com>
2020-04-25 18:58:05 +0200

    Gradle implementado

--- src/app/reservas.service.ts
+++ src/app/reservas.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {from, Observable} from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
-import { Reserv } from './reserv';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservasService {
-  private reservasUrl = ' ';  // URL to web api
-  constructor(private http: HttpClient) { }
-  getReservas(): Observable<Reserv[]> {
-    return this.http.get<Reserv[]>(this.reservasUrl);
+  private reservasUrl: string;  // URL to web api
+  constructor(private http: HttpClient) {
+    this.reservasUrl = 'http://localhost:8080/reservas';
+  }
+  public getReservasByEspacio() {
+    return this.http.get(this.reservasUrl + '/getReservasByEspacio');
+  }
+  public getReservasFiltradas() {
+    return this.http.get(this.reservasUrl + '/getReservasFiltradas');
+  }
+  public crearReserva() {
+   // return this.http.post<ReservaDTO>(this.reservasUrl + '/createReserva');
+  }
+  public cambiarEstado() {
+    // return this.http.patch(this.reservasUrl + '/changeState/{id}');
   }
 }
