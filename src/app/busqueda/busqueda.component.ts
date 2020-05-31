027410666764236d63c65e66eae60d5e8d55436f
Gonzalo Berne Melguizo <gera1397@gmail.com>
2020-05-31 02:08:46 +0200

    Actualizada página de busqueda de espacios

--- src/app/busqueda/busqueda.component.ts
+++ src/app/busqueda/busqueda.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-busqueda',
@@ -13,11 +13,37 @@ export class BusquedaComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<BusquedaComponent>) {
   }
   ngOnInit(): void {
+    this.habilitarDias(false);
   }
+
   onCancelClick(): void {
      this.dialogRef.close();
   }
+
   busqueda(): void{
 
   }
+
+  cambioPeriodo(event) {
+    if (event.target.checked) {
+      this.habilitarDias(true);
+    } else {
+      this.habilitarDias(false);
+    }
+  }
+
+  habilitarDias(periodo: boolean) {
+    $('#periodo1').prop('disabled', !periodo);
+    $('#periodo2').prop('disabled', !periodo);
+    $('#periodo3').prop('disabled', !periodo);
+    $('#periodo4').prop('disabled', !periodo);
+    $('#periodo5').prop('disabled', !periodo);
+    $('#periodo6').prop('disabled', !periodo);
+    $('#periodo7').prop('disabled', !periodo);
+  }
+
+  checkEquipamiento(event) {
+    $('#equipamientoNumero_' + event.target.id.split('_')[1]).prop('disabled', !event.target.checked);
+  }
+
 }
