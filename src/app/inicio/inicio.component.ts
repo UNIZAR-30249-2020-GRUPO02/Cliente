01b21fbcfb47b78ba2d17f1326ff5321f6e7d07d
Joaquin <joaquinpuyuelolardies@gmail.com>
2020-06-02 23:32:09 +0200

    Integración Open Street View en inicio, con imagen de ejemplo.

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -2,6 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 
+// Open layers imports
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import VectorLayer from 'ol/layer/Vector';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+
 
 import {BusquedaComponent} from '../busqueda/busqueda.component';
 import {LoginComponent} from '../auth/login/login.component';
@@ -13,10 +24,24 @@ import {LoginComponent} from '../auth/login/login.component';
 })
 export class InicioComponent implements OnInit {
 
+
   constructor(public router: Router, public matDialog: MatDialog) { }
 
   ngOnInit(): void {
+    var map = new Map({
+          target: 'hotel_map',
+          layers: [
+            new TileLayer({
+              source: new OSM()
+            })
+          ],
+          view: new View({
+            center: olProj.fromLonLat([7.0785, 51.4614]),
+            zoom: 5
+          })
+        });
   }
+
   goReservas() {
     const redirectUrl = '/reserva';
     // Redirect the user
