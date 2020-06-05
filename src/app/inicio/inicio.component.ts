70cf4ccd94ea24a2ce599b6fb94074201bf7fd3f
Joaquin <joaquinpuyuelolardies@gmail.com>
2020-06-05 10:30:23 +0200

    Update.

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -28,19 +28,32 @@ export class InicioComponent implements OnInit {
   constructor(public router: Router, public matDialog: MatDialog) { }
 
   ngOnInit(): void {
+    var wmsSource = new OSM({
+      url: 'http://91.250.168.62:8082/geoserver/web/wms',
+      params: {'LAYERS': 'ne:ne', 'TILED': true},
+      serverType: 'geoserver',
+      crossOrigin: 'anonymous'
+    });
+
+    var wmsLayer = new TileLayer({
+      source: wmsSource
+    });
+
+    var view = new View({
+      center: [0, 0],
+      zoom: 1
+    });
+
     var map = new Map({
-          target: 'hotel_map',
-          layers: [
-            new TileLayer({
-              source: new OSM()
-            })
-          ],
-          view: new View({
-            center: olProj.fromLonLat([7.0785, 51.4614]),
-            zoom: 5
-          })
-        });
+      layers: [wmsLayer],
+      target: 'map',
+      view: view
+    });
+
+
   }
+
+
 
   goReservas() {
     const redirectUrl = '/reserva';
