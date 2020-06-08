34caaeeb41e23e8f8ce152337fbab14e704d954d
Gonzalo Berne Melguizo <gera1397@gmail.com>
2020-06-09 01:19:37 +0200

    Procfile creado

--- src/app/server.js
+++ src/app/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(dirname + '/dist/<name-of-app>'));
+app.use(express.static(dirname + '/dist/smartcampus-cliente'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(dirname+'/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(dirname +'/dist/smartcampus-cliente/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
